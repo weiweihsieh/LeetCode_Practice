8bd0186e7d4370232536a55010513b714478c7e3
weiweihsieh <45007937+weiweihsieh@users.noreply.github.com>
2022-02-11 14:30:59 +0800

    Add files via upload

---
+++ 206_reverse_linked_list.cpp
@@ -0,0 +1,58 @@
+# include <iostream>
+
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution
+{
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        ListNode *prev = nullptr;
+        ListNode *current = head;
+        ListNode *next;
+        while(current != nullptr)
+        {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;            
+        }
+        
+        head = prev;
+    
+        return head;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    
+    //input
+    ListNode list(1);
+    list.next = new ListNode(6);
+    list.next->next = new ListNode(8);
+    
+    ListNode* result = sol.reverseList(&list);
+
+    //output
+    ListNode* trace = result;
+    while(trace != nullptr)
+    {
+        cout << trace->val;
+        trace = trace->next;
+    }
+    cout << endl;
+
+    return 0;
+}
