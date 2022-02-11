ad60696e1b14d1301b8a2f51d005b0c2bafcfa99
weiweihsieh <45007937+weiweihsieh@users.noreply.github.com>
2022-02-11 14:56:18 +0800

    Add files via upload

---
+++ 237_delete_node_in_a_linked_list.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    void deleteNode(ListNode* node)
+    {
+        node->val = node->next->val;
+        node->next = node->next->next;
+        
+        /*
+        ListNode *temp = node->next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        temp->next = NULL;
+        */
+    }
+};
+
+int main()
+{
+    Solution sol;
+    
+    //input
+    ListNode list(4);
+    list.next = new ListNode(5);
+    list.next->next = new ListNode(1);
+    list.next->next->next = new ListNode(9);
+    ListNode* node_delete = list.next; //given node
+
+    sol.deleteNode(node_delete);
+
+    //output
+    ListNode* trace = &list;
+    while(trace != nullptr)
+    {
+        cout << trace->val;
+        trace = trace->next;
+    }
+    cout << endl;
+
+    return 0;
+}
