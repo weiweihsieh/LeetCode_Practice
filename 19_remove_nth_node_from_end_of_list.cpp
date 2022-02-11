485f4b3334128d4f028e63bc4a8e989803956298
weiweihsieh <45007937+weiweihsieh@users.noreply.github.com>
2022-02-11 14:52:13 +0800

    Add files via upload

---
+++ 19_remove_nth_node_from_end_of_list.cpp
@@ -0,0 +1,69 @@
+# include <iostream>
+
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void printList(ListNode* head)
+{
+    while(head != nullptr)
+    {
+        cout << head->val;
+        head = head->next;
+    }
+}
+
+class Solution
+{
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n)
+    {
+        ListNode *fast = head;
+        ListNode *slow = nullptr; //previous node of delete
+        ListNode *delNode = nullptr;
+
+        ListNode dummy(-1, head); //dummy node
+        slow = &dummy;
+
+        for(int i=0; i<n; i++)
+            fast = fast->next;
+
+        while(fast != nullptr)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        delNode = slow->next;
+        slow->next = delNode->next;
+        delNode = NULL;
+
+        return dummy.next;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    
+    //input
+    ListNode a(1);
+    a.next = new ListNode(6);
+    a.next->next = new ListNode(8);
+
+    ListNode* n = sol.removeNthFromEnd(&a, 2);
+
+    //output
+    printList(n);
+    cout << endl;
+
+    return 0;
+}
