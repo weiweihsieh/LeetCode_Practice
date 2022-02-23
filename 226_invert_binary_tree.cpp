7e456f488b7e9877ad49c96199323077ba954fba
weiweihsieh <45007937+weiweihsieh@users.noreply.github.com>
2022-02-23 16:45:13 +0800

    Add files via upload

---
+++ 226_invert_binary_tree.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+//Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode* invertTree(TreeNode* root)
+    {
+        if(root == nullptr)
+            return nullptr;
+        
+        //swap
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        return root;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    
+    //input
+    TreeNode input(1);
+    input.left = new TreeNode(2);
+    input.left->left = new TreeNode(3);
+    input.right = new TreeNode(6);
+    input.right->right =  new TreeNode(4);
+
+    TreeNode* output = sol.invertTree(&input);
+    
+    //output
+    cout << output->left->val << endl;
+
+    return 0;
+}
