4b59e841737eae31465dd501508a9d18d5e4c002
weiweihsieh <45007937+weiweihsieh@users.noreply.github.com>
2022-03-11 17:14:43 +0800

    Add files via upload

---
+++ 24_swap_nodes_in_pairs.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (!head || !head->next)
+            return head;
+
+        ListNode *dummy = new ListNode(-1, head);
+        ListNode *prev = dummy, *curr = head, *next = head->next;
+
+        while (curr && curr->next)
+        {
+            next = curr->next;
+
+            prev->next = curr->next;
+            curr->next = next->next;
+            next->next = curr;
+
+            prev = curr;
+            curr = prev->next;
+        }
+
+        return dummy->next;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    // input
+    ListNode *list = new ListNode(1);
+    list->next = new ListNode(2);
+    list->next->next = new ListNode(3);
+    list->next->next->next = new ListNode(4);
+
+    ListNode *result = sol.swapPairs(list);
+
+    // output
+    ListNode *trace = result;
+    while (trace)
+    {
+        cout << trace->val;
+        trace = trace->next;
+    }
+    cout << endl;
+
+    return 0;
+}
